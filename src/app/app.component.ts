66c663a30f88bbe7a2e0ef9228be41004c3a8a03
Thao Trinh <admin@Thaos-MacBook-Pro.local>
2020-06-11 00:04:56 -0400

    use particles library for background

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ParticlesConfig } from './particles-config';
+
+declare let particlesJS: any; // Required to be properly interpreted by TypeScript.
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'portfolio';
-}
+export class AppComponent implements OnInit {
+  public ngOnInit(): void {
+    this.invokeParticles();
+  }
+
+  invokeParticles(): void {
+    particlesJS('particles-js', ParticlesConfig, function() {});
+  }}
